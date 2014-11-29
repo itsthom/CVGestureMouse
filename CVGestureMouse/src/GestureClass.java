5fe9d2c1dc57e00822c8cb50f8168cfc0419a685
Terabithia <boming.bj@gmail.com>
2014-11-29 00:59:42 -0500

    Fixed Peaks and Valleys(hopefully)

--- CVGestureMouse/src/GestureClass.java
+++ CVGestureMouse/src/GestureClass.java
@@ -36,6 +36,12 @@ public class GestureClass {
 		double peakThresh = 1.6; // positive angle threshold for peaks
 		double valleyThresh = -1.6; // negative angle threshold for valleys
 		
+		for(int i = 0; i < contour.length / 2; i++){
+			Point temp = contour[i];
+			contour[i] = contour[contour.length - i - 1];
+			contour[contour.length - i -1] = temp;
+		}
+		
 		for (int i = 0; i < contour.length; i++) {
 			int a, b;
 			if (i < k)
@@ -47,124 +53,70 @@ public class GestureClass {
 				b = (i + k) % k;
 			else
 				b = i+k;
-			
-//			Core.circle(img, contour[a], 20, new Scalar(255,0,0));
-//			Core.circle(img, contour[b], 20, new Scalar(255,0,0));
-			
-			double ay = contour[a].y - contour[i].y, 
-					ax = contour[a].x - contour[i].x,
+			double ay = contour[i].y - contour[a].y , 
+					ax = contour[i].x - contour[a].x,
 					by = contour[b].y - contour[i].y, 
 					bx = contour[b].x - contour[i].x;
 			angles[i] = Math.atan2(ax*by - ay*bx, ax*bx + ay*by); //perp dot product
 		}
 		
 		int K = 25;
-		double[] neighbors = new double[2 *K];
-		
+
 		for (int i = 0; i < angles.length; i++) {
-			if ((angles[i] < peakThresh && angles[i] > valleyThresh)) {
-				if (angles[i] > 0 ) {
-					int index;
-					
-//					for (int j = 0; j < K; j++) {
-//						if (i < K)
-//							index = angles.length - (K - j);
-//						else
-//							index = i - j;
-//						
-//						if (angles[index] > 0 && angles[index] < peakThresh)
-//							neighbors[j] = angles[index];
-//					}
-					
-					for (int j = 0; j < K; j++) {
-						if (i >= (angles.length - K))
-							index = i;//(i + j) % K;
-						else
-							index = i + j;
-						if (angles[index] > 0 && angles[index] < peakThresh)
-							neighbors[j + K] = angles[index];
-					}
-					
-					boolean isPeak = true;
-					for (int j=0; j < neighbors.length; j++) {
-						if (neighbors[j] != 0 && angles[i] > neighbors[j]) {
-							isPeak = false;
-							break;
+			if (angles[i] > peakThresh) {
+				boolean isMaxPeak = true;
+				int index = i - K;
+				for (int j = 0;j<2*K;j++){
+					if (index < 0){
+						if (angles[i] < angles[angles.length + index]){
+							isMaxPeak = false;
+						}
+					} else if (index > angles.length - 1){
+						if (angles[i] < angles[index - angles.length]){
+							isMaxPeak = false;
+						}
+					} else {
+						if (angles[i] < angles[index]){
+							isMaxPeak = false;
 						}
 					}
-					
-					if (isPeak){
-						Core.circle(img, contour[i], 15, new Scalar(255,0,0), 3);
-						peaks++;
-					}
-					
-					Core.circle(img, contour[i], 8, new Scalar(0,255,0),2);
-						valleys++;
-					
+					index++;
 				}
-//					
-//				if (angles[i] < 0 ) {
-//					Core.circle(img, contour[i], 20, new Scalar(0,255,0));
-//					valleys++;
-//				}
+				if (isMaxPeak){
+					Core.circle(img, contour[i], 15, new Scalar(255,0,0), 2);
+					peaks++;
+				}
 			}
-//			
-//			int K = 30;
-//			if (angles.length > K && (angles[i] > peakThresh && angles[i] < valleyThresh)) {
-//				// get neighborhood [i-k, i+k]
-//				double[] neighborAngles = new double[2 * K];
-//				int index;
-//				
-//				for (int j = -K; j < 0; j++) {
-//					if (i < K)
-//						index = angles.length - (K-i);
-//					else
-//						index = i - K;
-//					neighborAngles[j + K] = angles[index];
-//				}
-//				
-//				for (int j = K; j < neighborAngles.length; j++) {
-//					if (i >= (angles.length - K))
-//						index = (i + K) % K;
-//					else
-//						index = i + K;
-//					neighborAngles[j] = angles[index];
-//				}
-//				
-//				boolean isBestPeak = true, isBestValley = true;
-//				if (angles[i] < 0) {
-//					isBestValley = false;
-//					for (int j=0; j < neighborAngles.length; j++) {
-//						if (angles[i] < neighborAngles[j]) {
-//							isBestPeak = false;
-//							break;
-//						}
-//					}
-//				} else {
-//					isBestPeak = false;
-//					for (int j=0; j < neighborAngles.length; j++) {
-//						if (angles[i] > neighborAngles[j]) {
-//							isBestValley = false;
-//							break;
-//						}
-//					}
-//				}
-//				
-//				if (isBestPeak) {
-//					peaks++;
-////					Core.circle(img, contour[i], 20, new Scalar(255,0,0));
-//				}
-//				if (isBestValley) {
-//					valleys++;
-//					Core.circle(img, contour[i], 20, new Scalar(0,255,0));
-//					
-//				}
-//			}
+			
+			if (angles[i] < valleyThresh) {
+				boolean isMinValley = true;
+				int index = i - K;
+				for (int j = 0;j<2*K;j++){
+					if (index < 0){
+						if (angles[i] > angles[angles.length + index]){
+							isMinValley = false;
+						}
+					} else if (index > angles.length - 1){
+						if (angles[i] > angles[index - angles.length]){
+							isMinValley = false;
+						}
+					} else {
+						if (angles[i] > angles[index]){
+							isMinValley = false;
+						}
+					}
+					index++;
+				}
+				if (isMinValley){
+					Core.circle(img, contour[i], 15, new Scalar(0,255,0), 2);
+					valleys++;
+				}
+			}
 		}
 		
 		if (frameNo % 20 == 0) {
 			System.out.println("Perimeter length: " + contour.length + ";    Peaks: " + peaks + ";    Valleys: " + valleys);
-			System.out.println(Arrays.toString(neighbors));
+			//System.out.println(Arrays.toString(angles));
 		}
 		frameNo++;
 		return new GestureClass(peaks, valleys);
